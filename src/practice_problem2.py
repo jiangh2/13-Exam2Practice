8b793f2899679c0529ebfa443176cdae005dfa0d
jiangh2 <Ydong6688@>
2018-10-06 17:44:34 -0400

    fff

--- src/practice_problem2.py
+++ src/practice_problem2.py
@@ -193,7 +193,7 @@ def practice_problem2b(sequence):
             string = string + sequence[k][0]
     return string
     ####################################################################
-    # TODO: 4. Implement and test this function.
+    # DONE: 4. Implement and test this function.
     #     The testing code is already written for you (above).
     ####################################################################
     # DIFFICULTY AND TIME RATINGS (see top of this file for explanation)
